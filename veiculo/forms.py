f0087fd38e09edbccc123b0b85e2e815a87befe2
mateus1503 <mm15032004@mail.com>
2024-07-01 18:54:43 -0300

    Editado o add_veiculos

--- veiculo/forms.py
+++ veiculo/forms.py
@@ -6,3 +6,7 @@ class VeiculoForm(forms.ModelForm):
     class Meta:
         model = Veiculo
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(VeiculoForm, self).__init__(*args, **kwargs)
+        self.fields['cliente'].widget.attrs['disabled'] = True
